edf9405140047600536d598bd1af89580766e16e
Alexandra <alexadolph1701@gmail.com>
2025-10-23 11:46:33 +0000

    Gerði insert fyrir Event verkefnið

--- gagnasafnsfraedi_25/event/insert.sql
+++ gagnasafnsfraedi_25/event/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO events (name, date, location, description, ticket_price, time, tickets_available)
-VALUES ('')
+VALUES ('Improv Ísland', '2025-10-29', 'þjóðleikhúskjallarinn', 'Spuna leikrit frá Improv Ísland.', 3500, '20:00', 150), 
+        ('Ari Eldjárn: Áramótaskop 2025', '2025-12-05', 'Vesturgata 27 - Akranes IS', 'Ari Eldjárn býður þér að kveðja með sér enn eitt árið sem er að líða með uppistandi.', 8900, '21:00', 212),
+        ('Iceguys í Laugardalshöll', '2025-12-13', 'Laugardalshöll', 'Iceguys koma í Laugardalshöllina með skemmtilega sýningu.', 7990, '18:00', 1000),
+        ('Emmsjé Gauti á trúnó - tónleikar/uppistand', '2025-10-30', 'Hjallavegur 2 - Njarðvík IS', 'Emmsjé Gauti heldur tónleika og uppistand á trúnó.', 7900, '20:00', 127);
