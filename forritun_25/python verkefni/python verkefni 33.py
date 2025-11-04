09345d5baf09716703712a27c46d5c55f1421f25
Alexandra <alexadolph1701@gmail.com>
2025-11-04 13:06:01 +0000

    gerði python verkefni 33.

--- forritun_25/python verkefni/python verkefni 33.py
+++ forritun_25/python verkefni/python verkefni 33.py
@@ -1,13 +1,43 @@
 #Verkefni 33
 
 print ("'''''''''''Liður 1'''''''''''")
+counter = 0     #telur hvað mörg orð inni halda EKKI stafinn E
+ecounter = 0    #telur hvað mörg orð inni halda stafinn E
+wordcounter = 0 #telur fjölda orða 
 file = open("words (1).txt")
-teljari = 0 
+for line in file:
+    word = line.strip()
+    wordcounter += 1
+    if 'e' not in word:
+        counter += 1
+    if 'e' in word:
+        ecounter += 1
+
+print ("Stafurinn e er ekki í", counter,"orðum")
+print ("%.0f"%((ecounter/wordcounter)*100) + "% orða innihalda stafinn E")
+print ("orðin eru", wordcounter, "í heildina")
+
+print ("'''''''''''Liður 2'''''''''''")
+leita = str(input("Sláðu inn einhverja bókstafi: "))
+bcounter = 0
+file = open("words (1).txt")
+for line in file:
+    word = line.strip()
+    fann = False
+    for stafur in leita:
+        if stafur in word:
+            fann = True
+    if fann == False:
+        bcounter += 1
+
+print ("Orðin sem ekki innihaldastafina", leita, "eru", bcounter, "talsins")
+
+
+print ("'''''''''''Liður 3'''''''''''")
+stafir_counter = 0
+file = open("words (1).txt")
 for line in file:
     word = line.strip()
     for stafur in word:
-        if stafur == 'e':
-            teljari -= 1
-        else:
-            teljari += 1
-print ("Stafurinn E er ekki í", teljari, "orðum")
+        stafir_counter += 1
+print ("Það eru", stafir_counter, "stafir í öllu skjalinu")
