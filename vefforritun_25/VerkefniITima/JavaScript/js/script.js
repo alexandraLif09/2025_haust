7eecbd3ace89e9328d39e98a1dde52ff149c9d2c
Alexandra <alexadolph1701@gmail.com>
2025-11-04 10:53:04 +0000

    verkefni um JavaScript með lista og dictionary

---
+++ vefforritun_25/VerkefniITima/JavaScript/js/script.js
@@ -0,0 +1,23 @@
+const faridTilLond = [
+                        "Ísland",
+                        "Noregur",
+                        "Svíþjóð",
+                        "Danmörk",
+                        "Spánn",
+                        "Norður Makedónía",
+                        "Slóvenía",
+                        "Ítalía",
+                        "England",
+                        "Írland",
+                        "Prag"
+];
+
+const nordurMakedonia = {
+                            land: "Norður Makedónía",
+                            capital: "Skopje",
+                            currency: "Makedónsk denar",
+                            officialLanguage: "Makedónska",
+                            population: "1.792.000",
+                            president: "Gordana Siljanovska-Davkova",
+                            nationalAnthem: "Денес над Македонија"
+};
