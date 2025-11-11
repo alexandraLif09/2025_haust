65ce7a6af185c44a2b807abd6f1c34b03d23bc2c
Alexandra <alexadolph1701@gmail.com>
2025-11-11 09:12:43 +0000

    gerði python verkefni 35 og 36 í forritun 1

--- forritun_25/python verkefni/python verkefni 36.py
+++ forritun_25/python verkefni/python verkefni 36.py
@@ -1,10 +1,10 @@
-#Verkefni 35
+#Verkefni 36
 
-svar = str(input("Skráðu inn fornafn: "))   #Slær inn fornafn sem á að leita að
+svar = str(input("Skráðu inn eftirnafn: "))   #Slær inn eftirnafn sem á að leita að
 file = open("nofn (3).txt")     #opnar skrá
 listi = []          #Býr til tóman lista
 for line in file:   #les í gegnum allar línurnar í textaskránni
     word = line.strip()     #eyðir stýriskránni úr línu (t.d. enter merkjum)
     listi = word.split(" ")     #splittar nöfnum upp og setur þau í listann (bil aðganag)
-    if listi[0] == svar:    #ef fyrsta nafnið er sama og leitaorðið...
+    if svar == listi[-1]:    #ef seinasta nafnið í listanum er jafntog nafnið sem við slóum inn
         print (word)    #þá prentar hún nafnið
