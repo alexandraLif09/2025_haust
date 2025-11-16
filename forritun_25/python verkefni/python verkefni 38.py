3cbcb9bcf3c47091d58354ff0316eb0c19f3cb3c
Alexandra <alexadolph1701@gmail.com>
2025-11-16 22:25:36 +0000

    gerði python vrkefni 38 úr forritun 1

--- forritun_25/python verkefni/python verkefni 38.py
+++ forritun_25/python verkefni/python verkefni 38.py
@@ -1,2 +1,30 @@
 #Verkefni 38
 
+def summa():
+    samtals = 0
+    for i in range(0, len(listi)):
+        samtals += listi[i]
+    return samtals
+
+def safntidni():
+    summ = 0
+    safn = []
+    for i in range (0, len(listi)):
+        summ += listi[i]
+        safn.append(summ)
+    return safn
+
+def rada():
+    listi.sort()
+    return listi
+
+
+
+listi = []
+svar = int(input("Hvað villtu slá inn margar tölur? "))
+for i in range(0, svar):
+    listi.append(int(input("Sláðu inn tölu ")))
+print (listi)
+print ("Summan er: ", summa())
+print ("Safntíðnin er: ", safntidni())
+print ("listinn raðaður: ", rada())
