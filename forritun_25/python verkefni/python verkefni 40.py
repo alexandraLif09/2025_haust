d4f4237d9d7b8fe07f919076c5d4762d69125339
Alexandra <alexadolph1701@gmail.com>
2025-11-17 17:09:49 +0000

    gerði python verkefni 40 í forritun 1

---
+++ forritun_25/python verkefni/python verkefni 40.py
@@ -0,0 +1,18 @@
+#verkefni 40
+
+listie = []
+listi3a = []
+
+file = open("nofn (3).txt")     
+listi = []         
+for line in file:   
+    word = line.strip()     
+    if "e" in word:
+        listie.append(word)
+    if "a" in word[2]:
+        listi3a.append(word)
+        (len(word) > 2)
+
+print("Nöfnin sem innihalda stafinn e eru:", len(listie))
+print (listi3a)
+print ("Fjöldi nafna sem eru með a sem þriðja staf er:", len(listi3a))
