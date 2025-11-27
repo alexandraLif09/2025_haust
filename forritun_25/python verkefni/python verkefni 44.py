69fa114e13b8e219e08bb179931a98c975204a59
Alexandra <alexadolph1701@gmail.com>
2025-11-27 12:59:10 +0000

    Kláraði python verkefni 44 í forritun 1

--- forritun_25/python verkefni/python verkefni 44.py
+++ forritun_25/python verkefni/python verkefni 44.py
@@ -1,11 +1,23 @@
 #Verkefni 44
 
 file = open("verk44.txt")
-
-listi = []
-
 for line in file:
-    word = line.strip(" ")
+    listi = []
+    word = line.strip()
     listi = line.split(";")
-print (listi)
-print (word)
+    print (listi[0])
+
+print (".................................")
+
+svar = str(input("Sláðu inn leitarorð: "))
+k = False
+file = open("verk44.txt")
+for line in file:
+    listi = []
+    word = line.strip()
+    listi = line.split(";")
+    if svar == listi[0].strip():
+        print ((listi[1]).strip())
+        k = True
+if k == False:
+    print ("Fann ekki leitarorð")
